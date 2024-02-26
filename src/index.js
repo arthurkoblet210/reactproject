398a4d1fe9562e6e05b9e1e2cc839a37d9c2ded2
arthurkoblet210 <arthurkoblet210@gmail.com>
2024-02-26 05:35:30 -0500

    #3 redux fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React, { Suspense } from "react";
-// import ReactDOM from "react-dom";
-import {createRoot} from 'react-dom/client';
-import "./assets/scss/style.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 import Loader from "./layouts/loader/Loader";
-
+import React, { Suspense } from "react";
+// import ReactDOM from "react-dom";
+import {createRoot} from 'react-dom/client';
+import "./assets/scss/style.scss";
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
