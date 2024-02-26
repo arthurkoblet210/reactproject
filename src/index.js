1f628df6835e11a3abd834b0dd1e009bac7086b2
arthurkoblet210 <arthurkoblet210@gmail.com>
2024-02-26 06:26:19 -0500

    #5 fix error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import Loader from "./layouts/loader/Loader";
+import React, { Suspense } from "react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
-import Loader from "./layouts/loader/Loader";
-import React, { Suspense } from "react";
+
 // import ReactDOM from "react-dom";
 import {createRoot} from 'react-dom/client';
 import "./assets/scss/style.scss";
