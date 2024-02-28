56c2c242dd007c0c70742da63e0084b2007ee885
arthurkoblet210 <arthurkoblet210@gmail.com>
2024-02-28 02:08:02 -0500

    Add package

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@
 
 // import ReactDOM from "react-dom";
 
+import { HashRouter } from "react-router-dom";
+const rootElement = document.getElementById('root');
 import React, { Suspense } from "react";
-import App from "./App";
-import {createRoot} from 'react-dom/client';
 import "./assets/scss/style.scss";
 import Loader from "./layouts/loader/Loader";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 const rootElement = document.getElementById('root');
+import React, { Suspense } from "react";
+import App from "./App";
+import {createRoot} from 'react-dom/client';
 const root = createRoot(rootElement);
 
 root.render(
